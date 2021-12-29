73dc4f68aeaf4b0ac4e28d129b2a7796b45faea4
KikiAbdullah <kikirabdullah@gmail.com>
2021-12-29 12:17:20 +0700

    update cover 29des2021

---
+++ coba.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, minimal-ui" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png" />
+    <link rel="apple-touch-startup-image" href="images/apple-touch-startup-image-640x920.png" />
+    <title>niobe - mobile template</title>
+    <link rel="stylesheet" href="css/swiper.css" />
+    <link rel="stylesheet" href="css/style.min.css" />
+    <link href="https://fonts.googleapis.com/css?family=Assistant:200,400,700&display=swap" rel="stylesheet" />
+</head>
+
+<body id="mobile_wrap">
+    <div class="views">
+        <div class="view view-main">
+            <div class="pages">
+                <div data-page="index" class="page homepage">
+                    <div class="page-content">
+                        <!-- Slider -->
+                        <div class="swiper-container introslider">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide" style="background-image: url(images/cover/bg_mandala.png)">
+                                    <div class="slider-caption">
+                                        <div class="container">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <img src="images/cover/top.png" class="wedding-img" alt="" />
+                                                </div>
+                                                <div class="col-md-12">
+                                                    <div class="text-cover">
+                                                        <h5>Kepada Bapak/Ibu Saudara/i</h5>
+                                                        <h3>Kiki Rizka Abdullah</h3>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-12">
+                                                    <a href="home.html">
+                                                        <img src="images/ornament/btn_buka.png" style="width: 50%" alt="" />
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/swiper.min.js"></script>
+    <script src="js/jquery.custom.js"></script>
+</body>
+
+</html>
